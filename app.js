21acff953c502aefdfb146c135bd5cc6ac2d3586
TiagoDanin <TiagoDanin@outlook.com>
2017-12-02 13:01:37 -0300

    Update func:chPage

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict'
 
-var Client = require('github')
-var yaml = require('js-yaml');
-var fs   = require('fs');
+var Client  = require('github')
+var fs      = require('fs');
+var yaml    = require('js-yaml');
 var jformat = require('jformat');
 var hubdown = require('hubdown')
 
@@ -56,24 +56,53 @@ function chPage(github, owner, repo, yml) {
 	try {
 		var config = yaml.safeLoad(fs.readFileSync('cubohub.yml', 'utf8'));
 		var site = yaml.safeLoad(fs.readFileSync('defaut.html', 'utf8'));
-		hubdown('# Tiago\n\n-------\n\n`hello world`').then(readme_html => {
-			site = site.format({
-				title: config.title,
-				description: config.description,
-				lang: config.lang,
-				repo: repo,
-				size: config.size,
-				watchers: 100,
-				readme_html: readme_html.content
-			}, true)
-			fs.writeFileSync('index.html', site, 'utf8')
-		});
+		var readme_html = hubdown('# Tiago\n\n-------\n\n`hello world`')
 
-		//console.log(config);
-		//console.log(config.title)
-		//console.log(config.desc)
-		//console.log(config.template)
-		//console.log(config.links)
+		var info = {
+			id: repo.id,
+			private: repo.private,
+			title: config.title || repo.name,
+			repo: config.repo || repo.name,
+			name: repo.name,
+			full_name: config.full_name || repo.full_name,
+			homepage: repo.homepage,
+			description: config.description || repo.description,
+			language: config.language || repo.language,
+			size: config.size || repo.size,
+			watchers: repo.watchers || 0,
+			forks_count: repo.forks_count || 0,
+			fork: repo.forks_count || 0,
+			stargazers_count: repo.stargazers_count || 0,
+			stars: repo.stargazers_count || 0,
+			readme_html: readme_html || '',
+			iframe_html: iframe_html || '',
+			created_at: repo.created_at,
+			updated_at: repo.updated_at,
+			pushed_at: repo.pushed_at,
+			content_html: readme_html + iframe_html
+			html_url: repo.html_url,
+			git_url: repo.git_url,
+			ssh_url: repo.ssh_url,
+			clone_url: repo.clone_url,
+			svn_url: repo.svn_url,
+			default_branch: repo.default_branch,
+			license: repo.license,
+			forks: repo.forks,
+			open_issues: repo.open_issues,
+			open_issues_count: repo.open_issues_count,
+			has_issues: repo.has_issues,
+			has_projects: repo.has_projects,
+			has_downloads: repo.has_downloads,
+			has_wiki: repo.has_wiki,
+			has_pages: repo.has_pages,
+			mirror_url: repo.mirror_url,
+			archived: repo.archived,
+		}
+
+		site = site.format(info, true)
+
+		fs.writeFileSync('index.html', site, 'utf8')
+
 	} catch (e) {
 		console.log(e);
 	}
