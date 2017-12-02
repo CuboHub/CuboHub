f4bcef836a5dd971efe025b51647b499ddaded87
TiagoDanin <TiagoDanin@outlook.com>
2017-12-02 15:42:50 -0300

    Update App.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 'use strict'
 
-var Client  = require('github')
-var fs      = require('fs');
-var yaml    = require('js-yaml');
-var jformat = require('jformat');
-var hubdown = require('hubdown')
+var Client    = require('github')
+var fs        = require('fs');
+var yaml      = require('js-yaml');
+var request   = require("request-promise-native")
+var jformat   = require('jformat');
+var hubdown   = require('hubdown')
+var bytelabel = require('bytelabel')
 
 var github = new Client({
 	debug: false
@@ -52,11 +54,49 @@ function chUpdateFile(github, path, code, message, branch, owner, repo) {
 	})
 }
 
-function chPage(github, owner, repo, yml) {
+async function chPage(github, owner, repo_name, yml) {
 	try {
-		var config = yaml.safeLoad(fs.readFileSync('cubohub.yml', 'utf8'));
-		var site = yaml.safeLoad(fs.readFileSync('defaut.html', 'utf8'));
-		var readme_html = hubdown('# Tiago\n\n-------\n\n`hello world`')
+		var config = yaml.safeLoad(yml);
+		
+		var site = ''
+		if (config.template_raw) {
+			site = await request(config.template_raw)
+		} else {
+			site = yaml.safeLoad(fs.readFileSync('template/' + config.template + '.html', 'utf8'));
+		}
+
+		var readme_md = ''
+		if (config.readme || config.readme_file) {
+			var readme_data = await github.repos.getContent({
+				owner: owner,
+				repo: repo_name,
+				path: config.readme_file || 'README.md'
+			})
+			readme_md = Buffer.from(readme_data.data.content, 'base64').toString()
+		}
+
+		var iframe_html = ''
+		if (config.iframe || config.iframe_html) {
+			var iframe_data = await github.repos.getContent({
+				owner: owner,
+				repo: repo_name,
+				path: config.iframe_html || 'iframe.html'
+			})
+			iframe_html = Buffer.from(iframe_data.data.content, 'base64').toString()
+		}
+
+		var readme_html = await hubdown(readme_md)
+		readme_html = readme_html.content
+		var content_html = readme_html + iframe_html
+
+		var repo = await github.repos.get({
+			owner: owner,
+			repo: repo_name
+		})
+		repo = repo.data
+
+		var size_repo_kb = (config.size || repo.size) * 1024
+		var size_repo = bytelabel(size_repo_kb, {round: true})
 
 		var info = {
 			id: repo.id,
@@ -68,7 +108,8 @@ function chPage(github, owner, repo, yml) {
 			homepage: repo.homepage,
 			description: config.description || repo.description,
 			language: config.language || repo.language,
-			size: config.size || repo.size,
+			size: size_repo,
+			size_kb: size_repo_kb,
 			watchers: repo.watchers || 0,
 			forks_count: repo.forks_count || 0,
 			fork: repo.forks_count || 0,
@@ -79,7 +120,7 @@ function chPage(github, owner, repo, yml) {
 			created_at: repo.created_at,
 			updated_at: repo.updated_at,
 			pushed_at: repo.pushed_at,
-			content_html: readme_html + iframe_html
+			content_html: content_html,
 			html_url: repo.html_url,
 			git_url: repo.git_url,
 			ssh_url: repo.ssh_url,
@@ -101,8 +142,8 @@ function chPage(github, owner, repo, yml) {
 
 		site = site.format(info, true)
 
+		chUpdateFile(github, 'index.html', site, 'TEST: UP', 'master', owner, repo_name)
 		fs.writeFileSync('index.html', site, 'utf8')
-
 	} catch (e) {
 		console.log(e);
 	}
@@ -132,6 +173,16 @@ function chCheckXML(github, owner, repo) {
 	github.repos.getContent({
 		owner: owner,
 		repo: repo,
+		path: '.github/cubohub.xml'
+	}, function (err, res) {
+		if (res) {
+			yml = Buffer.from(res.data.content, 'base64').toString()
+			return chPage(github, owner, repo, yml)
+		}
+	})
+	github.repos.getContent({
+		owner: owner,
+		repo: repo,
 		path: 'cuboHub.xml'
 	}, function (err, res) {
 		if (res) {
@@ -152,17 +203,7 @@ function chCheckXML(github, owner, repo) {
 	github.repos.getContent({
 		owner: owner,
 		repo: repo,
-		path: '.cuboHub.xml'
-	}, function (err, res) {
-		if (res) {
-			yml = Buffer.from(res.data.content, 'base64').toString()
-			return chPage(github, owner, repo, yml)
-		}
-	})
-	github.repos.getContent({
-		owner: owner,
-		repo: repo,
-		path: '.CuboHub.xml'
+		path: '.cubohub.xml'
 	}, function (err, res) {
 		if (res) {
 			yml = Buffer.from(res.data.content, 'base64').toString()
@@ -171,11 +212,11 @@ function chCheckXML(github, owner, repo) {
 	})
 }
 
-//chPage(github, owner, repo, yml)
-chPage(github, 'TiagoDanin', 'TestGithub', 'yml')
-
 //chCheckXML(github, owner, repo)
-//chCheckXML(github, 'TiagoDanin', 'TestGithub')
+chCheckXML(github, 'TiagoDanin', 'TestGithub')
+
+//chPage(github, owner, repo, yml)
+//chPage(github, 'TiagoDanin', 'TestGithub', 'yml')
 
 //chUpdateFile(github, path, code, message, branch, owner, repo)
 //chUpdateFile(github, 'index.html', '<html><h1>Hello World!</h1></html>', 'TEST: UP', 'master', 'TiagoDanin', 'TestGithub')
