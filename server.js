3b493cd15f22953ad422601f49e8c0e6c71c67d2
TiagoDanin <TiagoDanin@outlook.com>
2017-12-03 10:27:27 -0300

    Update...

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const WebhooksApi = require('@octokit/webhooks')
 const webhooks = new WebhooksApi({
-	secret: 'mysecret'
+  secret: 'mysecret'
 })
 
-webhooks.on('push', ({id, name, payload}) => {
+
+console.log('[+] Start')
+webhooks.on('*', ({id, name, payload}) => {
 	console.log(name)
 	console.log(payload)
+	console.log('\n\n\n')
+	console.log(payload.repository.name)
+	console.log(payload.repository.owner.name)
 })
 
-require('http').createServer(webhooks.middleware).listen(3000)
+require('http').createServer(webhooks.middleware).listen(process.env.PORT)
+console.log('[+] Port ', process.env.PORT)
